c3435676be25c7f63333d1631b6ec91cd453308a
casizzy <scr38919@lasallebajio.edu.mx>
2025-09-27 18:24:08 -0600

    creaciónidk

--- app/src/main/java/stella/salle/scasillasloginapp/MainActivity.kt
+++ app/src/main/java/stella/salle/scasillasloginapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SCasillasLoginAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Andsdroid",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }

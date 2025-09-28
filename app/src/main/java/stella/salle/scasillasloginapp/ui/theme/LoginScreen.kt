2fcd40e022b7b1f75b406590c4b0ffa606e399ca
casizzy <scr38919@lasallebajio.edu.mx>
2025-09-27 18:34:05 -0600

    agregar previews

--- app/src/main/java/stella/salle/scasillasloginapp/ui/theme/LoginScreen.kt
+++ app/src/main/java/stella/salle/scasillasloginapp/ui/theme/LoginScreen.kt
@@ -1,2 +1,15 @@
 package stella.salle.scasillasloginapp.ui.theme
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewNews() {
+    SCasillasLoginAppTheme {
+        LoginScreen()
+    }
+}
